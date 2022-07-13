1fe37a7a7148e38ed4eb256ca6f3b07a64c49dde
Marcio Roberto <marcio.silva@keeggo.com>
2022-07-13 18:13:07 -0300

    Andamentos do Fundamentos

---
+++ OperadoresDeAtribuicao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class OperadoresDeAtribuicao
+    {
+     public static void Executar()
+        {
+            var num1 = 3;
+            num1 = 7;
+            num1 += 10; //num1 = num1 + 10;
+            num1 -= 3;  //num1 = num1 -3;
+            num1 *= 5;  //num1 = num1 *5;
+            num1 /= 2;  //num1 = num1 /2;
+
+            Console.WriteLine(num1);
+        }
+    }
+}
---
+++ OperadoresRelacionais.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class OperadoresRelacionais
+    {
+        public static void Executar()
+        {
+            double nota = 6.0;
+            double notaDecorte = 7.0;
+
+            Console.WriteLine("Nota inválida? {0}", nota >10.00);
+            Console.WriteLine("Nota inválida? {0}", nota < 00.00);
+            Console.WriteLine("Perfeirto? {0} ", nota == 10.00);
+            Console.WriteLine("Tem com Melhorar? {0}", nota != 10.00);
+            Console.WriteLine("Passou Por Média? {0}", nota >=notaDecorte);
+            Console.WriteLine("recuperação? {0}", nota < notaDecorte);
+            Console.WriteLine("Reprovado? {0} ", nota <= 3.0);
+        }
+    }
+}
