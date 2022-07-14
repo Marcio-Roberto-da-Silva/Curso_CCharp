417f798977bc8cfead32141feb241867c7ef94ca
Marcio Roberto <marcio.silva@keeggo.com>
2022-07-14 10:01:39 -0300

    Operadores

--- OperadoresDeAtribuicao.cs
+++ OperadoresDeAtribuicao.cs
@@ -16,6 +16,22 @@ namespace CursoCSharp.Fundamentos
             num1 /= 2;  //num1 = num1 /2;
 
             Console.WriteLine(num1);
+
+            int a = 1;
+            int b = a;
+
+            a++; // a = a + 1;
+            b--; // b = b - 1;
+
+            Console.WriteLine($"{a} {b}");
+
+            // Não se preucupe com código
+
+            dynamic c = new System.Dynamic.ExpandoObject();
+            c.nome = "Maria";
+
+            Console.WriteLine(c.nome);
+
         }
     }
 }
